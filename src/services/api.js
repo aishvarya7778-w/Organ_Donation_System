cef370f5d11e150a118332466affdeecba9a73ed
Aishvarya <aishvarya7778@gmail.com>
2026-05-16 14:14:55 +0530

    Fixed production api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://organ-backend-iemp.onrender.com',
+  baseURL: 'https://organ-backend-iemp.onrender.com/api',
 });
 
 const getPathname = (url) => {
