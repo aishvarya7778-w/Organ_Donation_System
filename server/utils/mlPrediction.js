8c409104e3f2a81c433094db4d493896440b6dff
Aishvarya <aishvarya7778@gmail.com>
2026-05-16 13:55:55 +0530

    Connected deployed ML Service

--- server/utils/mlPrediction.js
+++ server/utils/mlPrediction.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const ML_SERVICE_URL = process.env.ML_SERVICE_URL || 'http://localhost:8000/predict';
+const ML_SERVICE_URL = process.env.ML_SERVICE_URL || 'http://https://organ-ml-service.onrender.com/predict';
 
 const BLOOD_GROUP_MAP = {
   O_PLUS: 0,
